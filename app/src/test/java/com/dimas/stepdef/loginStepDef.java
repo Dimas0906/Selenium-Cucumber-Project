cabe15c3da1db41f5606431d11455886e6ff0b23
BrainCell-404 <braincellno404@gmail.com>
2024-06-18 21:47:28 +0700

    Add Gherkin, Stepdef and Page object for Login

--- app/src/test/java/com/dimas/stepdef/loginStepDef.java
+++ app/src/test/java/com/dimas/stepdef/loginStepDef.java
@@ -3,14 +3,42 @@ package com.dimas.stepdef;
 import com.dimas.BaseTest;
 import com.dimas.page.loginPage;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class loginStepDef extends BaseTest {
   loginPage loginPage;
 
-  @Given("I am on the login page")
+  @Given("User on the login page")
   public void userIsOnLoginPage() {
     loginPage = new loginPage(driver);
     loginPage.goToLoginPage();
+  }
+
+  @And("User input username and key in {string}")
+  public void userInputUsername(String username) {
+    loginPage.inputUsername(username);
+  }
+
+  @And("User input password and key in {string}")
+  public void userInputPassword(String password) {
+    loginPage.inputPassword(password);
+  }
+
+  @When("User click on the login button")
+  public void userClickLoginButton() {
+    loginPage.clickLoginButton();
+  }
+
+  @Then("User should be redirected to the home page")
+  public void userShouldBeRedirectedToHomePage() {
+    assert driver.getCurrentUrl().equals("https://www.saucedemo.com/inventory.html");
+  }
+
+  @Then("User able to see error message {string}")
+  public void userShouldSeeAnErrorMessage(String errMsg) {
+    loginPage.getErrorMessage(errMsg);
   }
 }
